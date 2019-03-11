bd1e572cc160e17ee47f8ae93c40d1e1589935f2
Sergey <xxsmotur@gmail.com>
2019-03-11 18:28:21 +0500

    shard: pushing data to the linked list

--- shard.go
+++ shard.go
@@ -14,6 +14,12 @@ type shard struct {
 	l *list.List
 }
 
+// entry represents d-s for inserting to linked list
+type entry struct {
+	key   []byte
+	value []byte
+}
+
 // newShard creates a new shard
 func newShard() *shard {
 	return &shard{
@@ -22,10 +28,11 @@ func newShard() *shard {
 }
 
 // set provides inserting of the data
-func set(key, value []byte) error {
+func (s *shard) set(key, value []byte) error {
 	if len(key) > maxKeySize {
 		return errKeyTooLarge
 	}
 
+	s.l.PushFront(&entry{key, value})
 	return nil
 }
