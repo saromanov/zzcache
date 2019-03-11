bc6a17dfead4499282fdd3aabd17fcc798f8eed2
Sergey <xxsmotur@gmail.com>
2019-03-11 17:23:11 +0500

    shard:implementation of the set method

--- shard.go
+++ shard.go
@@ -1,5 +1,21 @@
 package zzcache
 
+import "errors"
+
+const maxKeySize = 65535
+
+var errKeyTooLarge = errors.New("key is too large")
+
 // shard provides implementation of the shard for cache
 type shard struct {
+
+}
+
+// set provides inserting of the data
+func set(key, value []byte) error {
+	if len(key) > maxKeySize {
+		return errKeyTooLarge
+	}
+
+	return nil
 }
